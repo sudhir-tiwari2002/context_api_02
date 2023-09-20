167aa6430739eb85664b42b80b45deba28e604b4
sudhir.pw <skt2ktiwari@gmail.com>
2023-09-20 15:30:51 +0530

    enabling modes by using the usecontext hook

---
+++ src/context/theme.jsx
@@ -0,0 +1,16 @@
+import React, { createContext, useContext } from "react";
+
+
+export const ThemeContext = createContext({
+    themeMode: 'light',
+    darkTheme:()=>{},
+    lightTheme:()=>{}
+}) // setting initial value of the context 
+
+
+export const ThemeProvider = ThemeContext.Provider
+
+
+export default function useTheme(){
+    return useContext(ThemeContext) 
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode:'class',// if we need to set mode into the react app then set the mode here
   theme: {
     extend: {},
   },
